4d64ae878c633275f72fcde122a7659c4aedaa2b
Joshua Munn <public@elysee-munn.family>
2023-11-21 21:56:41 +0000

    Add command line interface to compiler

---
+++ compiler/__main__.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+import subprocess
+import sys
+import tempfile
+
+from compiler import *
+
+project_dir = pathlib.Path(__file__).parent.parent
+
+
+if len(sys.argv) == 2 and sys.argv[1] in ("-p", "--print-asm"):
+    emit_program(
+        eval(sys.stdin.read().replace("\n", "").replace("\r", "")),
+        StdoutWriter().write,
+    )
+else:
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        asmfile = os.path.join(tmpdirname, "stst.s")
+        binfile = os.path.join(tmpdirname, "stst")
+        runtimefile = project_dir / "startup.c"
+        with Writer(asmfile) as writer:
+            emit_program(
+                eval(sys.stdin.read().replace("\n", "").replace("\r", "")),
+                writer.write,
+            )
+        subprocess.run(
+            ["gcc", "-fomit-frame-pointer", runtimefile, asmfile, "-o", binfile],
+            check=True,
+        )
+        print(
+            ">",
+            subprocess.run([binfile], check=True, capture_output=True).stdout.decode(
+                "utf-8"
+            ),
+            end="",
+        )
