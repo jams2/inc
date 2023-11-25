2f3a468e656fe88d71fff95e2690809adaa29281
Joshua Munn <public@elysee-munn.family>
2023-11-25 10:17:44 +0000

    Tidy up cli

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pathlib
 import subprocess
@@ -8,31 +9,77 @@ from compiler import *
 
 project_dir = pathlib.Path(__file__).parent.parent
 
+builtins = {
+    "λ": "lambda",
+    "letrec": "letrec",
+    "app": "app",
+    "f": "f",
+    "g": "g",
+    **{c: Var(c) for c in ("x", "y", "t")},
+}
 
-if len(sys.argv) == 2 and sys.argv[1] in ("-p", "--print-asm"):
-    emit_program(
-        eval(sys.stdin.read().replace("\n", "").replace("\r", "")),
-        StdoutWriter().write,
-    )
-else:
-    sys.modules[__name__].__dict__.update({c: Var(c) for c in ("x", "y", "t")})
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        asmfile = os.path.join(tmpdirname, "stst.s")
-        binfile = os.path.join(tmpdirname, "stst")
-        runtimefile = project_dir / "startup.c"
-        with Writer(asmfile) as writer:
-            emit_program(
-                eval(sys.stdin.read().replace("\n", "").replace("\r", "")),
-                writer.write,
+sys.modules[__name__].__dict__.update(builtins)
+
+
+parser = argparse.ArgumentParser(prog="compiler")
+output_group = parser.add_mutually_exclusive_group(required=True)
+output_group.add_argument(
+    "-x",
+    "--execute",
+    required=False,
+    default=True,
+    action="store_true",
+    help="compile and execute program",
+)
+output_group.add_argument(
+    "-p", "--print", required=False, action="store_true", help="print listing"
+)
+input_group = parser.add_mutually_exclusive_group(required=True)
+input_group.add_argument(
+    "-f", "--file", required=False, type=str, help="source file path"
+)
+input_group.add_argument(
+    "-i",
+    "--read-stdin",
+    required=False,
+    action="store_true",
+    dest="read_stdin",
+    help="read from stdin",
+)
+
+
+def main():
+    args = parser.parse_args()
+    if args.read_stdin:
+        program = sys.stdin.read().replace("\n", "").replace("\r", "")
+    else:
+        with open(args.file, "r") as f:
+            program = f.read()
+
+    if args.print:
+        emit_program(eval(program), StdoutWriter().write)
+    else:
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            asmfile = os.path.join(tmpdirname, "stst.s")
+            binfile = os.path.join(tmpdirname, "stst")
+            runtimefile = project_dir / "startup.c"
+            with Writer(asmfile) as writer:
+                emit_program(
+                    eval(program),
+                    writer.write,
+                )
+            subprocess.run(
+                ["gcc", "-fomit-frame-pointer", runtimefile, asmfile, "-o", binfile],
+                check=True,
             )
-        subprocess.run(
-            ["gcc", "-fomit-frame-pointer", runtimefile, asmfile, "-o", binfile],
-            check=True,
-        )
-        print(
-            ">",
-            subprocess.run([binfile], check=True, capture_output=True).stdout.decode(
-                "utf-8"
-            ),
-            end="",
-        )
+            print(
+                ">",
+                subprocess.run(
+                    [binfile], check=True, capture_output=True
+                ).stdout.decode("utf-8"),
+                end="",
+            )
+
+
+if __name__ == "__main__":
+    main()
--- gdb_pretty_printer.py
+++ gdb_pretty_printer.py
@@ -1,4 +1,13 @@
+import string
+
 import gdb
+
+CHARS = string.ascii_letters + string.punctuation + string.whitespace + string.digits
+CHARSHIFT = 8
+CHARTAG = 0x0F
+CHARMASK = 0x3F
+
+chars = {(ord(c) << CHARSHIFT) | CHARTAG for c in CHARS}
 
 
 class CustomValuePrinter:
@@ -14,13 +23,15 @@ class CustomValuePrinter:
             return "#t"
         elif self.val == 0x3F:
             return "()"
+        elif (self.val & CHARMASK) == CHARTAG:
+            return chr(self.val >> CHARSHIFT)
 
 
 def custom_value_lookup_function(val):
     if (
         val.type.strip_typedefs().code == gdb.TYPE_CODE_INT
         and val.type.sizeof == 8
-        and val in (0x2F, 0x6F, 0x3F)
+        and val in (0x2F, 0x6F, 0x3F, *chars)
     ):
         return CustomValuePrinter(val)
     return None
